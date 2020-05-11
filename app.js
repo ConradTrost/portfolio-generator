e6848e6a0b58768eeb6e376bbbc0f88c83ef8cf2
Conrad Trost <ctrost@tamu.edu>
2020-05-11 17:27:06 -0500

    Added user input using npm inquirer

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 // import fs module from node library
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const [name, github] = profileDataArgs;
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// const pageHTML = generatePage(portfolioData)
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw err;
 
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt ([
+        {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your Github username?'
+    }])
+    .then(answers => {
+        console.log(answers);
+    })
+    .catch(error => {
+        if(error.isTtyError) {
+
+        } else {
+
+        }
+    });
